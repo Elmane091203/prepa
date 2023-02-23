5faa12c12da965b0afb24926a03788ed87216475
bahalphasouleymane <bahalphasouleymane2019@gmail.com>
2023-02-23 00:55:23 +0000

    prepa devoir

---
+++ src/entity/Etudiant.java
@@ -0,0 +1,82 @@
+package entity;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Etudiant {
+    private int id ;
+    private String matricule ;
+    private String nom ;
+    private String prenom ;
+    private double moyenne ;
+    private Classe classe;
+    //
+    public Etudiant() {
+    }
+
+    public Etudiant(String nom, String prenom, double moyenne, Classe classe) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.moyenne = moyenne;
+        this.classe = classe;
+        this.matricule = generateMatricule();
+    }
+
+    //
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public double getMoyenne() {
+        return moyenne;
+    }
+
+    public void setMoyenne(double moyenne) {
+        this.moyenne = moyenne;
+    }
+
+    public Classe getClasse() {
+        return classe;
+    }
+
+    public void setClasse(Classe classe) {
+        this.classe = classe;
+    }
+    public String generateMatricule()
+    {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        String date = dateFormat.format(new Date());
+        String mat = "ET@"+date+getClasse().getNom()+"#";
+        return  mat ;
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import entity.Classe;
+import entity.Etudiant;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Classe classe = new Classe("Gl");
+        System.out.println("nom est classe est:" + classe.getNom());
+        Etudiant etudiant = new Etudiant();
+        etudiant.setId(1);
+        etudiant.setNom("NDEYE");
+        etudiant.setPrenom("NAFISA");
+        etudiant.setMoyenne(18.5);
+        etudiant.setClasse(classe);
+        etudiant.setMatricule(etudiant.generateMatricule());
+        System.out.println(etudiant.getNom()+ etudiant.getPrenom()+etudiant.getMoyenne()+etudiant.getMatricule());
+
+        //etudiant.getClasse().getNom();
+    }
+}
